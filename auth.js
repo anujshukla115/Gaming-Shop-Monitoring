fa0448f78479d8d15e44516e04f4c7b9ab1536ce
Anuj Shukla <anujshukla2431527@gmail.com>
2026-07-11 14:23:59 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'https://finflow-expense-tracker-backend.vercel.app/api';
+const API_URL = 'https:///api';
 
 // For local development, use:
 // const API_URL = window.location.hostname === 'localhost' || 
